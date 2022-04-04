a3fbf045b700618bc344f51b26e2b2140a320e35
Ralph Kilian <rkilian1@my.bcit.ca>
2022-04-04 13:57:52 -0700

    hide gmaps api key

--- pages/api/geocode.js
+++ pages/api/geocode.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const geocode = async (req, res) => {
   const { place_id } = req.body
   const { data } = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?place_id=${place_id}&key=AIzaSyA4oWhR283IIhxIPn0_k7W8_JlmupbY0a4`
+    `https://maps.googleapis.com/maps/api/geocode/json?place_id=${place_id}&key=${process.env.GOOGLE_MAPS_API_KEY}`
   )
 
   const { location } = data.results[0].geometry
